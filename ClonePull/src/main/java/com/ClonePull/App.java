d3e377b73c43275b73d38ee1ace33ffea099f463
root <maheshwaramshweta1@gmail.com>
2024-12-05 14:27:57 +0530

    Class is created

---
+++ ClonePull/src/main/java/com/ClonePull/App.java
@@ -0,0 +1,13 @@
+package com.ClonePull;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
